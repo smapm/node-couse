2f824d8ef9565fbf482e2c293c576116fd7f096d
sarath sm <61122522+smapm@users.noreply.github.com>
2020-03-16 18:36:38 +0530

    Update notes.js

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,69 @@
-const notes = () =>{
-    return 'my notes...'
+const fs = require('fs');
+const chalk = require('chalk');
+
+const addNote = (title, body) => {
+    const notes = loadNote();
+    debugger
+    let duplicate = notes.find((note) => note.title === title);
+    if(!duplicate){
+        let data = {title: title, body: body};
+        notes.push(data);
+        saveNote(notes);
+    }
+    else{
+        console.log(chalk.red.inverse('Duplicate!!!'));
+    }
 };
 
-module.exports =notes;
+const loadNote = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json').toString();
+        return JSON.parse(dataBuffer);
+    } catch (e) {
+        return [];
+    }
+
+};
+
+const saveNote = (notes) => { 
+    fs.writeFileSync('notes.json', JSON.stringify(notes));
+};
+
+const removeNote = (title) =>{
+    let notes = loadNote();
+    let filterdNotes = notes.filter( (note) =>{
+        return note.title !== title;
+    });
+    if(filterdNotes.length === notes.length){
+        console.log(chalk.red('Note not exist!!'));
+    }else{
+        saveNote(filterdNotes);
+        console.log(chalk.green('Note removed!!'));
+    }
+}
+
+const listNotes = () =>{
+    let notes = loadNote();
+    console.log(chalk.yellow.inverse("------------ notes ---------------"));
+    notes.forEach(element => {
+        console.log(element.title);
+    });
+}
+
+const readNote = (title) =>{
+    let notes = loadNote();
+    let note = notes.find( (val) => val.title === title);
+    if(note){
+        console.log(chalk.inverse.green(note.title));
+        console.log(note.body);
+    }else{
+        console.log(chalk.inverse.red('Note not found'));
+    }
+}
+
+module.exports = {
+    addNote : addNote,
+    removeNote : removeNote,
+    listNotes : listNotes,
+    readNote : readNote
+};
