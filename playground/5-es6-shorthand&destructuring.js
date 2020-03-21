cb5e9910caa0ca1d0a9d3751d597ab50c6891d87
smapm <smsarath1234@gmail.com>
2020-03-22 01:04:33 +0530

    callback chain

---
+++ playground/5-es6-shorthand&destructuring.js
@@ -0,0 +1,8 @@
+const obj = {
+    name: 'sarath',
+    age: 25,
+    sex: 'male'
+};
+
+const{...data} = obj;
+console.log(data);
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,28 +1,23 @@
-const request = require('request');
+const geocode = require('./utils/geocoding');
+const forecast = require('./utils/forecast');
 
-const url = 'https://api.darksky.net/forecast/ef60cff5b82d5745cf8902d20b88d6e9/37.8267,-122.4233';
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic21hcG0iLCJhIjoiY2s4MWFoamxzMDVhbTNmcGdreGdpbnFndCJ9.ggOjrcXDp44-WRkM3Cp-XA&limit=1';
+const address = process.argv[2];
 
-request({url : url, json:true}, (error, response) =>{
+if(!address){
+    return console.log('Please provide address');
+    
+}
+
+geocode(address, (error, locationInfo) =>{
     if(error){
-        console.log('unable to connect to weather service')
+       return console.log(error);
     }
-    else if(response.body.error){
-        console.log('Unable to find location');
-    }else{
-        const data = response.body.currently;
-        console.log(`it is currently ${data.temperature} degree and ${data.precipProbability}% chance of rain`);
-    }   
-});
-
-request({url : geoUrl, json: true}, (error, response) =>{
-    if(error){
-        console.log('unable to connect to location service')
-    }
-    else if(response.body.features.length === 0){
-        console.log('Incorrect location provided');
-    }else{
-    const data = response.body.features[0].center
-    console.log(`latitude ${data[1]}, longitude ${data[0]}`)
-    }
+    const {latitude, longitude, location} = locationInfo;
+    forecast(latitude, longitude, (error, data) =>{
+        if(error){
+            return console.log( error);
+        }
+        console.log(location);
+        console.log(data);
+    });
 });
