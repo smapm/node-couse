89739a48ceed928882d68a1b4376d46702a6c646
smapm <smsarath1234@gmail.com>
2020-03-26 20:49:58 +0530

    complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,11 @@ const path = require('path');
 const hbs = require('hbs');
 const express = require('express');
 const app = express();
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocoding');
 
-app.listen(3000, () =>{
-    console.log('server up and running!');  
+app.listen(3000, () => {
+    console.log('server up and running!');
 });
 
 const rootdir = path.join(__dirname, '../public');
@@ -18,44 +20,59 @@ hbs.registerPartials(partialsdir)
 
 app.use(express.static(rootdir))
 
-app.get('', (req, res) =>{
-    res.render('index',{
-        title : 'Weather',
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
         name: 'Sarath'
     });
 })
 
-app.get('/about', (req, res) =>{
-    res.render('about',{
-        title : 'About me',
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
         name: 'Sarath'
     });
 })
 
-app.get('/help', (req, res) =>{
-    res.render('help',{
-        title : 'Help',
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
         name: 'Sarath'
     });
 })
 
-app.get('/weather',(req, res) =>{
-    if(!req.query.address){
-        return res.send({error: "Address must be provided"})
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({ error: "location must be provided" })
     }
-    res.send({name: 'sarath'})
+    let address = req.query.address;
+    geocode(address, (error, locationInfo) => {
+        if (error) {
+            return res.send({ error });
+        }
+        const { latitude, longitude, location } = locationInfo;
+        forecast(latitude, longitude, (error, data) => {
+            if (error) {
+                return res.send({ error });
+            }
+            res.send({
+                location,
+                forecast: data
+            })
+        });
+    });
 })
 
-app.get('/help/*', (req, res) =>{
-   res.render('404',{
-    title : 'Help article not found',
-    name: 'Sarath'   
-   })
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Help article not found',
+        name: 'Sarath'
+    })
 })
 
-app.get('*', (req, res) =>{
-    res.render('404',{
-        title : '404 Not found'
-       })
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 Not found'
+    })
 })
 
